d832c31cc644987ebe85f99414729cbfdd1e80c8
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2015-09-07 09:24:08 -0500

    Add tests for Multiplication.

--- exercise_1.17.exs
+++ exercise_1.17.exs
@@ -12,3 +12,22 @@ defmodule Multiplication do
   defp double(a), do: 2 * a
   defp halve(a), do: div(a, 2)
 end
+
+ExUnit.start
+
+defmodule MultiplicationTests do
+  use ExUnit.Case, async: true
+
+  test "it multiplies by 0" do
+    assert Multiplication.mult(5, 0) == 0
+    assert Multiplication.mult(0, 12) == 0
+    assert Multiplication.mult(0, 0) == 0
+  end
+
+  test "it multplies numbers" do
+    assert Multiplication.mult(5, 5) == 25
+    assert Multiplication.mult(4, 7) == 28
+    assert Multiplication.mult(7, 14) == 98
+    assert Multiplication.mult(8, 8) == 64
+  end
+end
